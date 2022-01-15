138ea70ee75de38361d02a8ea032d0f7c8202ef8
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-15 18:26:23 -0500

    created counter component

---
+++ test-practicing/src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleIncrementClick() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function handleDecrementClick() {
+    if (count === 0) {
+      setCount(0);
+    } else {
+      setCount((prevCount) => prevCount - 1);
+    }
+  }
+
+  return (
+    <div className="container">
+      <p className="container_count">{count}</p>
+      <div className="container_count_btn">
+        <button className="btn" onClick={handleDecrementClick}>
+          Dislike
+        </button>
+        <button className="btn" onClick={handleIncrementClick}>
+          Like
+        </button>
+      </div>
+    </div>
+  );
+}
