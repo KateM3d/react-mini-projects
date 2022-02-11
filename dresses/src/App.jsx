8b8677cbbc5fb34d204bd3ec7aefa55b4b5d087a
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-10 20:07:39 -0500

    added filter

--- dresses/src/App.jsx
+++ dresses/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { data } from "./data";
 import "./App.css";
-import FirstName from "./FirstName";
+import Clothes from "./Clothes";
+import Button from "./Button";
 
 export default function App() {
-  const [image, setImage] = useState(data);
+  const [clothes, setSlothes] = useState(data);
+
+  const sortedClothes = (searchTerm) => {
+    const newClothes = data.filter((el) => el.searchTerm === searchTerm);
+    setSlothes(newClothes);
+  };
+
   return (
     <div>
-      <FirstName dog={image} />
+      <div className="cont">
+        <h2 className="back">Free Standard Shipping</h2>
+      </div>
+      <Button handleClick={sortedClothes} />
+      <Clothes info={clothes} />
     </div>
   );
 }
