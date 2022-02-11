80c7fe4e7c9b05bfe8dfc30a63ab3a1e7e53184f
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-11 13:37:10 -0500

    added react router

--- dresses/src/App.jsx
+++ dresses/src/App.jsx
@@ -1,24 +1,43 @@
-import { useState } from "react";
-import { data } from "./data";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import Clothes from "./Clothes";
-import Button from "./Button";
+import About from "./components/About";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Team from "./components/Team";
+import ContactUs from "./components/ContactUs";
 
 export default function App() {
-  const [clothes, setSlothes] = useState(data);
-
-  const sortedClothes = (searchTerm) => {
-    const newClothes = data.filter((el) => el.searchTerm === searchTerm);
-    setSlothes(newClothes);
-  };
-
   return (
-    <div>
-      <div className="cont">
-        <h2 className="back">Free Standard Shipping</h2>
-      </div>
-      <Button handleClick={sortedClothes} />
-      <Clothes info={clothes} />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/about" className="link">
+          About
+        </Link>
+        <Link to="/home" className="link">
+          Home
+        </Link>
+        <Link to="/shop" className="link">
+          Shop
+        </Link>
+        <Link to="/team" className="link">
+          Team
+        </Link>
+        <Link to="/contact" className="link">
+          Contact Us
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
   );
 }
---
+++ dresses/src/components/Shop.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { data } from "../data";
+import Clothes from "./Clothes";
+import Button from "./Button";
+
+export default function Shop() {
+  const [clothes, setSlothes] = useState(data);
+
+  const sortedClothes = (searchTerm) => {
+    const newClothes = data.filter((el) => el.searchTerm === searchTerm);
+    setSlothes(newClothes);
+  };
+
+  return (
+    <div>
+      <div className="cont">
+        <h2 className="back">Free Standard Shipping</h2>
+      </div>
+      <Button handleClick={sortedClothes} />
+      <Clothes info={clothes} />
+    </div>
+  );
+}
